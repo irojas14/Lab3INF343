33797d88f79cfde809ca3266ed1b77228e1fd0ba
irojas14 <ignacio.rojas.14@sansano.usm.cl>
2021-12-07 18:11:22 -0300

    Se agregan funciones para crear archivos

--- Funciones/funcs.go
+++ Funciones/funcs.go
@@ -1,6 +1,10 @@
 package funcs
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	pb "github.com/irojas14/Lab3INF343/Proto"
 )
 
@@ -17,4 +21,57 @@ func CmdToStr(cmd pb.TipoComando) string {
 		r = "DeleteCity"
 	}
 	return r
+}
+
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return (err != nil)
+}
+
+func CrearRegistroPlanetario(NombreDelArchivo string) {
+	// check if file exists
+	var _, err = os.Stat(NombreDelArchivo)
+
+	// create file if not exists
+	if os.IsNotExist(err) {
+		var file, err = os.Create(NombreDelArchivo)
+		if isError(err) {
+			return
+		}
+		defer file.Close()
+	}
+
+	fmt.Println("Se crea el archivo llamado: ", NombreDelArchivo)
+}
+
+//Hay que editar esta función para adaptarla a esta tarea
+func InsertarComandoEnRegistro(NombreDelArchivo string, JugadasDelJugador []int32) {
+	// Open file using READ & WRITE permission.
+	var file, err = os.OpenFile(NombreDelArchivo, os.O_APPEND|os.O_WRONLY, 0644)
+	if isError(err) {
+		return
+	}
+	defer file.Close()
+
+	for _, jugada := range JugadasDelJugador {
+		// Hacer algo
+		// Write some text line-by-line to file.
+		var linea string = FormatInt32(jugada) + "\n"
+		_, err = file.WriteString(linea)
+		if isError(err) {
+			return
+		}
+	}
+
+	err = file.Sync()
+	if isError(err) {
+		return
+	}
+}
+
+func FormatInt32(n int32) string {
+	return strconv.FormatInt(int64(n), 10)
 }
